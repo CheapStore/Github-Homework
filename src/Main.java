7c068a80675c891ae9126a3da8c1e417e7b76ab3
Sirojiddin <orinboyevsirojiddin@gmail.com>
2023-11-25 18:38:03 +0500

    5commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
 
     }
 }
